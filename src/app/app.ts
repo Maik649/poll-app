4d62385cbf6066e65221ef60c62b562123445d8c
Maik Pankow <maikpankow@gmail.com>
2026-05-15 00:19:11 +0200

    feat: add header and hero section, varieablen.scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { HeroSecton } from './shared/components/hero-secton/hero-secton';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, HeroSecton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/hero-secton/hero-secton.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-secton',
+  imports: [],
+  templateUrl: './hero-secton.html',
+  styleUrl: './hero-secton.scss',
+})
+export class HeroSecton {}
