7899432b5cd0d74d1643f9e08427f50398aff2be
Maik Pankow <maikpankow@gmail.com>
2026-05-15 01:02:11 +0200

    feat: add Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroSecton } from './shared/components/hero-secton/hero-secton';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeroSecton,
+    },
+];
